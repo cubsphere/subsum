cb300af14bd678adcc5e123f727bfc5a075adb1d
cubsphere <6753972+cubsphere@users.noreply.github.com>
2018-12-05 12:14:22 -0300

    central algorithm complete

--- src/subsum.cpp
+++ src/subsum.cpp
@@ -44,47 +44,85 @@ bool out_of_bounds(long sum, long *Y, long setsize, long subsetsize, long depth,
     long row = subsetsize - card - 1;
     long lower_bound = sum + Yaccess(Y, setsize, row, depth);
     long upper_bound = sum + Yaccess(Y, setsize, row, setsize - row - 1);
-    return target < lower_bound || target > lower_bound;
+    return target < lower_bound | target > upper_bound;
 }
 
-void subsum(long *set, long setsize, long *subset, long subsetsize, long target)
+static const bool LEFT = true;
+static const bool RIGHT = false;
+
+long subsum(long *set, long setsize, long subsetsize, long target, vector<unsigned long> *solutions)
 {
     long Ysize = cumulative_sum(setsize);
     long *Y = new long[Ysize];
     computeY(Y, set, setsize);
 
     long leaves = 0;
-    dynamic_bitset<> status(setsize);
+    dynamic_bitset<> status(setsize+1);
     long depth = 0;
     long sum = 0;
     long card = 0;
 
-    status[0] = true;
+    status.set();
+    status[setsize] = RIGHT;
     bool go_left = true;
 
     while (1)
     {
-        status <<= 1;
         if (go_left)
         {
             ++depth;
+            status[setsize - depth] = LEFT;
         }
         else
         {
             sum += set[depth];
             ++depth;
             ++card;
-            status[0] = true;
+            status[setsize - depth] = RIGHT;
         }
+        string s;
+        to_string(status, s);
 
         if (sum == target & card == subsetsize)
         {
+            status.flip();
+            solutions->push_back(status.to_ulong());
+            status.flip();
         }
-        else if (card == subsetsize || out_of_bounds(sum, Y, setsize, subsetsize, depth, card, target))
+
+        if (depth == setsize)
         {
+            ++leaves;
+            long return_to = status.find_first();
+            if(return_to == -1)
+                break;
+            long newdepth = setsize - return_to;
+            for(long i = newdepth; i < depth; ++i)
+            {
+                sum -= set[i];
+            }
+            card -= depth - newdepth;
+            depth = newdepth - 1;
+            go_left = false;
+        } else if (card == subsetsize || out_of_bounds(sum, Y, setsize, subsetsize, depth, card, target))
+        {
+            ++leaves;
+            long return_to = status.find_next(setsize - depth - 1);
+            if(return_to == -1)
+                break;
+            long newdepth = setsize - return_to;
+            for(long i = newdepth; i < depth; ++i)
+            {
+                sum -= set[i];
+            }
+            card -= depth - newdepth;
+            depth = newdepth - 1;
+            go_left = false;
         }
         else
         {
+            go_left = true;
         }
     }
+    return leaves;
 }
