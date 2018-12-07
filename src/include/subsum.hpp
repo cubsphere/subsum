34275e815fd27c0dbb048c5547ebd34541880753
cubsphere <6753972+cubsphere@users.noreply.github.com>
2018-12-07 07:57:58 -0300

    changed to double, added header

---
+++ src/include/subsum.hpp
@@ -0,0 +1,10 @@
+#ifndef SUBSUM
+#define SUBSUM
+
+#include <vector>
+
+using namespace std;
+
+long subsum(double *set, long setsize, long subsetsize, double target, vector<unsigned long> *solutions);
+
+#endif
--- src/subsum.cpp
+++ src/subsum.cpp
@@ -1,9 +1,12 @@
 #include <vector>
+#include <math.h>
 #include <string.h>
 #include <boost/dynamic_bitset.hpp>
 
 using namespace std;
 using namespace boost;
+
+const double EPSILON = ((double) 1) / ((double) (1<<16));
 
 long cumulative_sum(long x)
 {
@@ -15,51 +18,56 @@ long poscalc(long setsize, long y)
     return cumulative_sum(setsize) - cumulative_sum(setsize - y);
 }
 
-long Yaccess(long *Y, long setsize, long y, long x)
+double Yaccess(double *Y, long setsize, long y, long x)
 {
     return Y[poscalc(setsize, y) + x];
 }
 
-void Ywrite(long *Y, long setsize, long y, long x, long val)
+void Ywrite(double *Y, long setsize, long y, long x, double val)
 {
     Y[poscalc(setsize, y) + x] = val;
 }
 
-void computeY(long *Y, long *set, long setsize)
+void computeY(double *Y, double *set, long setsize)
 {
-    memcpy(Y, set, setsize * sizeof(long));
+    memcpy(Y, set, setsize * sizeof(double));
     for (long i = 1; i < setsize; ++i)
     {
         for (long j = 0; j < setsize - i; ++j)
         {
-            long sum = Yaccess(Y, setsize, i - 1, j);
+            double sum = Yaccess(Y, setsize, i - 1, j);
             sum += Yaccess(Y, setsize, 0, j + 1);
             Ywrite(Y, setsize, i, j, sum);
         }
     }
 }
 
-bool out_of_bounds(long sum, long *Y, long setsize, long subsetsize, long depth, long card, long target)
+bool double_compare(double a, double b)
+{
+    return fabs(a - b) < EPSILON;
+}
+
+bool out_of_bounds(double sum, double *Y, long setsize, long subsetsize, long depth, long card, double target)
 {
     long row = subsetsize - card - 1;
-    long lower_bound = sum + Yaccess(Y, setsize, row, depth);
-    long upper_bound = sum + Yaccess(Y, setsize, row, setsize - row - 1);
-    return target < lower_bound | target > upper_bound;
+    double lower_bound = sum + Yaccess(Y, setsize, row, depth);
+    double upper_bound = sum + Yaccess(Y, setsize, row, setsize - row - 1);
+    return (target + EPSILON < lower_bound) | (target - EPSILON > upper_bound);
 }
 
 static const bool LEFT = true;
 static const bool RIGHT = false;
 
-long subsum(long *set, long setsize, long subsetsize, long target, vector<unsigned long> *solutions)
+long subsum(double *set, long setsize, long subsetsize, double target, vector<unsigned long> *solutions)
 {
     long Ysize = cumulative_sum(setsize);
-    long *Y = new long[Ysize];
+    double *Y = new double[Ysize];
     computeY(Y, set, setsize);
 
     long leaves = 0;
     dynamic_bitset<> status(setsize+1);
     long depth = 0;
-    long sum = 0;
+    double sum = 0;
     long card = 0;
 
     status.set();
@@ -83,11 +91,22 @@ long subsum(long *set, long setsize, long subsetsize, long target, vector<unsign
         string s;
         to_string(status, s);
 
-        if (sum == target & card == subsetsize)
+        if (double_compare(sum, target) & card == subsetsize)
         {
             status.flip();
             solutions->push_back(status.to_ulong());
             status.flip();
+            /*//solution checker
+            long l = 0;
+            for(long i = 0; i < setsize; ++i)
+            {
+                if(!((bool) status[setsize - 1 - i]))
+                {
+                    l += set[i];
+                }
+            }
+            l += 1;
+            */
         }
 
         if (depth == setsize)
